964e9e23a4e7632ec7f508d7544aafa5ebc61b5d
Jason Morley <Jason.Morley@zibrant.com>
2018-04-10 14:55:42 +0100

    Now exposes maybe operators in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as Generators from "./iterable/generators";
 import * as Iterables from "./iterable/operators";
+import * as Maybe from "./maybe";
+import * as Maybes from "./maybe/operators";
 import * as Promises from "./promise/operators";
 
 import { $$ } from "./pipe";
@@ -8,5 +10,8 @@ export {
 	Iterables,
 	Generators,
 	Promises,
+	Maybes,
+	Maybe,
 	$$
 };
+export type Maybe<T> = Maybe.Maybe<T>;
