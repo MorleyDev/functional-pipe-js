37dca4c1d2bbc23f42e2a8f65bef0560d5afb5bc
Jason Morley <Jason.Morley@zibrant.com>
2018-04-10 14:56:47 +0100

    Release v2.0.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,8 @@
 import * as Generators from "./iterable/generators";
 import * as Iterables from "./iterable/operators";
+import * as Maybe from "./maybe";
+import * as Maybes from "./maybe/operators";
 import * as Promises from "./promise/operators";
 import { $$ } from "./pipe";
-export { Iterables, Generators, Promises, $$ };
+export { Iterables, Generators, Promises, Maybes, Maybe, $$ };
+export declare type Maybe<T> = Maybe.Maybe<T>;
